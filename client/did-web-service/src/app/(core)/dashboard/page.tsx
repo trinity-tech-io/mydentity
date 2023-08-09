ca660255f269d2b12019dbd21add8c59d7d30f01
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2023-08-09 18:07:42 +0800

    Hide useless date picker from dashboard

--- client/did-web-service/src/app/(core)/dashboard/page.tsx
+++ client/did-web-service/src/app/(core)/dashboard/page.tsx
@@ -1,7 +1,4 @@
 'use client'
-import { Datepicker } from '@components/Datepicker';
-import FilterButton from '@components/DropdownFilter';
-import { MainButton } from '@components/MainButton';
 import { FC } from "react";
 import { WelcomeBanner } from "./WelcomeBanner";
 import { IdentityListWidget } from "./widgets/IdentityList";
@@ -19,18 +16,15 @@ const Dashboard: FC = () => {
       <div className="sm:flex sm:justify-between sm:items-center mb-8">
 
         {/* Right: Actions */}
-        <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
-          {/* Filter button */}
+        {/* <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
           <FilterButton align="left" />
-          {/* Datepicker built with flatpickr */}
           <Datepicker />
-          {/* Add view button */}
           <MainButton title='Add view' leftIcon={
             <svg className="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
               <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
             </svg>
           } />
-        </div>
+        </div> */}
       </div>
     </div>
 
