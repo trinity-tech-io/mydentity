8c672b79f28e142f8a28499c1cf1719247a863f3
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2023-08-24 15:57:45 +0800

    Reworked sidebar to keep it open while loading server page

--- client/did-web-service/src/app/partials/Sidebar.tsx
+++ client/did-web-service/src/app/partials/Sidebar.tsx
@@ -91,7 +91,8 @@ const groups: GroupConfig[] = [
 
 const LinkElement: FC<{
   link: LinkConfig;
-}> = ({ link }) => {
+  sidebarExpanded: boolean;
+}> = ({ link, sidebarExpanded }) => {
   const pathname = usePathname();
   const isActive = link.url === pathname;
 
@@ -100,7 +101,7 @@ const LinkElement: FC<{
       <Link
         href={link.url}
         className={clsx('block transition duration-150 truncate', isActive ? 'text-indigo-500' : 'text-slate-400 hover:text-slate-200')}>
-        <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+        <span className={clsx("text-sm font-medium lg:opacity-0 2xl:opacity-100 duration-200", sidebarExpanded && "lg:opacity-100")}>
           {link.title}
         </span>
       </Link>
@@ -110,8 +111,9 @@ const LinkElement: FC<{
 
 const GroupElement: FC<{
   group: GroupConfig;
+  sidebarExpanded: boolean;
   onGroupHeaderClicked: () => void;
-}> = ({ group, onGroupHeaderClicked }) => {
+}> = ({ group, sidebarExpanded, onGroupHeaderClicked }) => {
   const { icon, title, links, requiresAuth = false } = group;
   const pathname = usePathname();
   const isActive = group.url === pathname || group.links?.some(l => l.url === pathname);
@@ -141,11 +143,10 @@ const GroupElement: FC<{
             <div className="flex items-center justify-between">
               <div className="flex items-center">
                 <Image src={icon} alt="" className='shrink-0 h-6 w-6' />
-                <span className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                <span className={clsx("text-sm font-medium ml-3 lg:opacity-0 2xl:opacity-100 duration-200", sidebarExpanded && "lg:opacity-100")}>
                   {title}
                 </span>
               </div>
-              {/* Icon */}
               {links &&
                 <div className="flex shrink-0 ml-2">
                   <svg className={`w-3 h-3 shrink-0 ml-1 fill-current text-slate-400 ${open && 'rotate-180'}`} viewBox="0 0 12 12">
@@ -157,9 +158,9 @@ const GroupElement: FC<{
           </Link>
           {/* Inner links */}
           {
-            links && <div className="lg:hidden lg:sidebar-expanded:block 2xl:block">
+            links && <div className={clsx("lg:hidden 2xl:block", sidebarExpanded && "lg:block")}>
               <ul className={`pl-9 mt-1 ${!open && 'hidden'}`}>
-                {links.map(link => <LinkElement key={link.title} link={link} />)}
+                {links.map(link => <LinkElement key={link.title} link={link} sidebarExpanded={sidebarExpanded} />)}
               </ul>
             </div>
           }
@@ -173,13 +174,11 @@ const Sidebar: FC<{
   sidebarOpen: boolean;
   setSidebarOpen: (open: boolean) => void;
 }> = ({ sidebarOpen, setSidebarOpen }) => {
-  const [authUser] = useBehaviorSubject(authUser$());
-
   const trigger = useRef<any>(null);
   const sidebar = useRef<any>(null);
-
   const [sidebarExpanded, setSidebarExpanded] = useState(true);
   let [activeIdentity] = useBehaviorSubject(activeIdentity$);
+
   // close on click outside
   useEffect(() => {
     const clickHandler: EventListener = (e) => {
@@ -201,13 +200,13 @@ const Sidebar: FC<{
     return () => document.removeEventListener('keydown', keyHandler);
   });
 
-  useEffect(() => {
+  /* useEffect(() => {
     if (sidebarExpanded) {
       document.querySelector('body')?.classList.add('sidebar-expanded');
     } else {
       document.querySelector('body')?.classList.remove('sidebar-expanded');
     }
-  }, [sidebarExpanded]);
+  }, [sidebarExpanded]); */
 
   return (
     <div>
@@ -222,8 +221,7 @@ const Sidebar: FC<{
       <div
         id="sidebar"
         ref={sidebar}
-        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-slate-800 p-4 transition-all duration-200 ease-in-out ${sidebarOpen ? 'translate-x-0' : '-translate-x-64'
-          }`}
+        className={clsx("flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 2xl:!w-64 shrink-0 bg-slate-800 p-4 transition-all duration-200 ease-in-out", sidebarOpen ? 'translate-x-0' : '-translate-x-64', sidebarExpanded && "lg:!w-64")}
       >
         {/* Sidebar header */}
         <div className="flex justify-between mb-10 pr-3 sm:px-2">
@@ -277,22 +275,22 @@ const Sidebar: FC<{
 
         {activeIdentity && <>
           <h3 className="text-xs uppercase text-slate-500 font-semibold pl-3">
-            <span className="hidden lg:block lg:sidebar-expanded:hidden 2xl:hidden text-center w-6" aria-hidden="true">
+            <span className={clsx("hidden lg:block 2xl:hidden text-center w-6", sidebarExpanded && "lg:hidden")} aria-hidden="true">
               •••
             </span>
-            <span className="lg:hidden lg:sidebar-expanded:block 2xl:block">ACTIVE IDENTITY</span>
+            <span className={clsx("lg:hidden 2xl:block", sidebarExpanded && "lg:!block")}>ACTIVE IDENTITY</span>
           </h3>
-          <span className="lg:hidden lg:sidebar-expanded:block 2xl:block text-slate-200 mx-3 mb-6" style={{ fontSize: 11 }}>{shortenDID(activeIdentity.did)}</span>
+          <span className={clsx("lg:hidden 2xl:block text-slate-200 mx-3 mb-6", sidebarExpanded && "lg:!block")} style={{ fontSize: 11 }}>{shortenDID(activeIdentity.did)}</span>
         </>
         }
 
         {/* Links */}
         <div /* className="space-y-8" */>
-
           {/* Link groups */}
           {groups.map(group => <GroupElement
             key={group.title}
             group={group}
+            sidebarExpanded={sidebarExpanded}
             onGroupHeaderClicked={() => {
               if (!sidebarExpanded)
                 setSidebarExpanded(true);
@@ -304,7 +302,7 @@ const Sidebar: FC<{
           <div className="px-3 py-2">
             <button onClick={() => setSidebarExpanded(!sidebarExpanded)}>
               <span className="sr-only">Expand / collapse sidebar</span>
-              <svg className="w-6 h-6 fill-current sidebar-expanded:rotate-180" viewBox="0 0 24 24">
+              <svg className={clsx("w-6 h-6 fill-current", sidebarExpanded && "rotate-180")} viewBox="0 0 24 24">
                 <path className="text-slate-400" d="M19.586 11l-5-5L16 4.586 23.414 12 16 19.414 14.586 18l5-5H7v-2z" />
                 <path className="text-slate-600" d="M3 23H1V1h2z" />
               </svg>
--- client/did-web-service/tailwind.config.js
+++ client/did-web-service/tailwind.config.js
@@ -62,13 +62,5 @@ module.exports = {
     },
   },
   plugins: [
-    // eslint-disable-next-line global-require
-    //require('@tailwindcss/forms'),
-    // add custom variant for expanding sidebar
-    plugin(({ addVariant, e }) => {
-      addVariant('sidebar-expanded', ({ modifySelectors, separator }) => {
-        modifySelectors(({ className }) => `.sidebar-expanded .${e(`sidebar-expanded${separator}${className}`)}`);
-      });
-    }),
   ],
 }
