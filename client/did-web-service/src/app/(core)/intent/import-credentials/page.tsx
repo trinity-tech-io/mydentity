699fba6b6d692b881fc666d6c0bd158ff2076430
snackwang <snackwang.petter@gmail.com>
2023-08-26 15:21:40 +0800

    Update import credential page

--- client/did-web-service/src/app/(core)/intent/import-credentials/page.tsx
+++ client/did-web-service/src/app/(core)/intent/import-credentials/page.tsx
@@ -3,20 +3,113 @@ import { MainButton } from "@components/MainButton";
 import { VerifiableCredential } from "@elastosfoundation/did-js-sdk";
 import { useBehaviorSubject } from "@hooks/useBehaviorSubject";
 import { Intent } from "@model/intent/intent";
+import { JSONObject } from "@model/json";
+import { Avatar, Stack, Typography } from "@mui/material";
 import { activeIdentity$ } from "@services/identity/identity.events";
 import { fetchIntent, fulfilIntentRequest } from "@services/intent.service";
 import { setQueryParameter } from "@utils/urls";
 import { FC, useEffect, useState } from "react";
+import { CredentialAndDetailComponent } from "../components/CredentialAndDetailComponent";
 import { PreparingRequest } from "../components/PreparingRequest";
+
+// Displayable version of a verifiable credential subject entry (a credential can contain several information
+// in its subject).
+export type ImportedCredentialItem = {
+  name: string,
+  value: string
+}
+
+// Displayable version of a verifiable credential. Can contain one or more ImportedCredentialItem that
+// are displayable version of verifiable credential subject entries.
+export type ImportedCredential = {
+  name: string,
+  values: ImportedCredentialItem[],
+  credential: VerifiableCredential,
+}
 
 const RequestDetails: FC<{
   intent: Intent<VerifiableCredential[]>;
 }> = ({ intent }) => {
   const [activeIdentity] = useBehaviorSubject(activeIdentity$);
   const [preparingResponse, setPreparingResponse] = useState(false);
-  const payload = intent.requestPayload;
+  const [importedCredentials, setImportedCredentials] = useState<ImportedCredential[]>(null);
+  const [wrongTargetDID, setWrongTargetDID] = useState<boolean>(false);
+  const [requestingAppIconUrl, setRequestingAppIconUrl] = useState<string>('');
+  const [requestingAppName, setRequestingAppName] = useState<string>('');
 
-  console.log("payload", payload)
+  const payload = intent.requestPayload;
+  useEffect(() => {
+    runPreliminaryChecks();
+    getDappIcon();
+    fetchApplicationDidInfo();
+    if (payload){
+      const importedCredentials = organizeImportedCredentials(payload.credentials)
+      setImportedCredentials(importedCredentials);
+    }
+  },[payload]);
+
+  /**
+   * Check a few things after entering the screen. Mostly, imported credentials content quality.
+   */
+  const runPreliminaryChecks = () => {
+    //TODO
+    setWrongTargetDID(false);
+  }
+
+  const getDappIcon = () => {
+    //TODO
+    setRequestingAppIconUrl("");
+  }
+
+  const fetchApplicationDidInfo = () => {
+    //TODO
+    setRequestingAppName('Demo app')
+  }
+
+  /**
+   * From the raw list of credentials provided by the caller, we create our internal model
+   * ready for UI.
+   * NOTE: We can have several credentials passed at the same time. Each credential can have several entries in its subject.
+   */
+  const organizeImportedCredentials = (credentialObjList: JSONObject[]): ImportedCredential[] => {
+    if (!credentialObjList)
+      return[];
+
+    let displayableCredentials = [];
+    credentialObjList.map((credentialObj: JSONObject) => {
+      const verifiableCredential = VerifiableCredential.parse(credentialObj.toString());
+      let credentialSubject = verifiableCredential.getSubject().getProperties();
+
+      // Generate a displayable version of each entry found in the credential subject
+      let displayableEntries: ImportedCredentialItem[] = [];
+      for (let subjectEntryKey of Object.keys(credentialSubject)) {
+        let subjectEntryValue = credentialSubject[subjectEntryKey];
+
+        if (subjectEntryKey == "id") // Don't display the special subject id entry
+          continue;
+
+        if (subjectEntryKey == "displayable") // Don't display the special subject displayable entry
+          continue;
+
+        let displayableEntry: ImportedCredentialItem = {
+          name: subjectEntryKey,
+          value: subjectEntryValue.toString(),
+        }
+
+        displayableEntries.push(displayableEntry);
+      }
+
+      let displayableCredential: ImportedCredential = {
+        name: verifiableCredential.getId().getFragment(),
+        values: displayableEntries,
+        credential: verifiableCredential,
+      };
+
+      displayableCredentials.push(displayableCredential);
+    });
+
+    return displayableCredentials;
+  }
 
   // User approves the upcoming request - data will be returned to the calling dApp.
   const approveRequest = async () => {
@@ -38,12 +131,39 @@ const RequestDetails: FC<{
   }
 
   return <>
-    Do you want to save the following content issued by app XXX, to your identity? This credential will not be
+    {/* Do you want to save the following content issued by app XXX, to your identity? This credential will not be
     visible by anyone unless you choose share it later.
     <br /><br />
     Intent: {intent.id}
-    <br /><br />
-    {activeIdentity && <MainButton onClick={approveRequest} busy={preparingResponse}>Import this to my profile</MainButton>}
+    <br /><br /> */}
+    {activeIdentity &&
+      <div className=" text-center">
+        <Stack direction="row" justifyContent="center">
+        <Typography>
+        </Typography>
+        {/* {requestingAppIconUrl && <Avatar src={requestingAppIconUrl} sx={{ ml:2, width: 120, height: 120 }}/>}
+        {!requestingAppIconUrl && <Avatar src={requestingAppIconUrl} sx={{ ml:2, width: 120, height: 120 }}/>} */}
+        <Avatar sx={{ ml:2, width: 120, height: 120 }}/>}
+        </Stack>
+        <Typography mt={4}>
+              {requestingAppName}
+        </Typography>
+
+        <Typography mt={4}>
+              You are going to attach some infomation provided by a third party to your identity.
+        </Typography>
+        <Typography mt={1}>
+              Please review the following data:
+        </Typography>
+        { importedCredentials?.map(importedCredential => {
+          return(
+            <CredentialAndDetailComponent key={importedCredential.name+"credentialanddetail"} importedCredential={importedCredential}/>
+          )
+        })}
+        <MainButton onClick={approveRequest} busy={preparingResponse} >Import this to my profile</MainButton>
+      </div>
+    }
+
     {!activeIdentity && "Make an identity active to continue"}
   </>
 }
@@ -65,7 +185,6 @@ const ImportCredentialsIntent: FC<{
     fetchIntent<VerifiableCredential[]>(requestId).then(_intent => {
       setLoadingIntent(false);
       setIntent(_intent);
-      console.log('intent result', _intent)
     });
   }, [searchParams?.rid]);
 
--- client/did-web-service/src/model/intent/request-payload.ts
+++ client/did-web-service/src/model/intent/request-payload.ts
@@ -1,4 +1,6 @@
+import { JSONObject } from "@model/json";
 
 export type IntentRequestPayload<T> = T & {
   caller: string; // Informative DID of the calling app
+  credentials: JSONObject[]
 }
