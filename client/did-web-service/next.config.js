2b02a1fa65ae8b026ef5c41b6da3b7ad89549d2e
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2023-09-05 16:51:40 +0800

    Fixed rdf-canonize-native error

--- client/did-web-service/next.config.js
+++ client/did-web-service/next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
+  webpack(config, { webpack }) {
     config.module.rules.push({
       test: /\.svg$/,
       issuer: /\.[jt]sx?$/,
       use: ["@svgr/webpack"]
     });
+
+    // Exclude the optional "rdf-canonize-native" lib used by jsonld, works only in nodejs environments.
+    config.plugins.push(new webpack.IgnorePlugin({
+      resourceRegExp: /rdf-canonize-native/
+    }));
+
     return config;
   }
 }
