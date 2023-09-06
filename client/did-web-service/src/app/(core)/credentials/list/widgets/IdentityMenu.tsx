31d231e387f85e737bbd8506b306649c1020e38b
liaihong <>
2023-09-06 16:53:49 +0800

    Fix error for handleOK on IdentityMenu

--- client/did-web-service/src/app/(core)/credentials/list/widgets/IdentityMenu.tsx
+++ client/did-web-service/src/app/(core)/credentials/list/widgets/IdentityMenu.tsx
@@ -75,7 +75,7 @@ const IdentityMenu: FC<{
   };
 
   // Edit -> Cancel/OK
-  const handleOK = async (editCredentialValue: { info: ProfileCredentialInfo, value: string, type: EditionMode, originCredential: ProfileCredential }): Promise<void> => {
+  const handleOK = async (editCredentialValue: { info: ProfileCredentialInfo, value?: string, type: EditionMode, originCredential: ProfileCredential, selectedDate?: Date }): Promise<void> => {
     setOpenEditCredentialDialog(false);
     if (!editCredentialValue)
       return;
