0db235d2472da20339694af6c9526baeb2274731
liaihong <>
2023-09-13 12:14:20 +0800

    Modify the default icon email color to white

--- client/did-web-service/src/model/credential/profile-info-icons.tsx
+++ client/did-web-service/src/model/credential/profile-info-icons.tsx
@@ -5,7 +5,7 @@ import FingerPrintIcon from '@assets/images/fingerprint.svg';
 export const defaultProfileIcons:{[profileInfoKey: string]: JSX.Element} = {
     'name': <AccountIcon style={{ width: 35, height: 35 }}/>,
     'gender': <AccountIcon style={{ width: 35, height: 35 }}/>,
-    'email': <EmailPrintIcon style={{ width: 35, height: 35 }}/>,
+    'email': <EmailPrintIcon style={{ width: 35, height: 35 , color:'white'}}/>,
     'birthDate': <AccountIcon style={{ width: 35, height: 35 }}/>,
     'fingerPrint': <FingerPrintIcon style={{ width: 35, height: 35 }}/>, // TODO: TEST
     'default':<AccountIcon style={{ width: 35, height: 35 }}/>   
