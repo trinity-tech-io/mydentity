d848ecbb0f85ed82f4d174a90bda8c55a90eb0ca
liaihong <>
2023-09-13 12:54:54 +0800

    Use default icon on the Profile page such as phone/nickname etc. to avoid display inconsistency.

--- client/did-web-service/src/model/credential/profile-info-icons.tsx
+++ client/did-web-service/src/model/credential/profile-info-icons.tsx
@@ -4,10 +4,30 @@ import FingerPrintIcon from '@assets/images/fingerprint.svg';
 
 export const defaultProfileIcons:{[profileInfoKey: string]: JSX.Element} = {
     'name': <AccountIcon style={{ width: 35, height: 35 }}/>,
-    'gender': <AccountIcon style={{ width: 35, height: 35 }}/>,
     'email': <EmailPrintIcon style={{ width: 35, height: 35 , color:'white'}}/>,
     'birthDate': <AccountIcon style={{ width: 35, height: 35 }}/>,
-    'fingerPrint': <FingerPrintIcon style={{ width: 35, height: 35 }}/>, // TODO: TEST
+    'nationality': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'gender': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'telephone': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'nickname': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'birthPlace': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'occupation': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'education': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'interests': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'description': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'url': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'discord': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'linkedin': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'facebook': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'instagram': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'twitter': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'snapchat': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'telegram': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'wechat': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'weibo': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'twitch': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'elaAddress': <AccountIcon style={{ width: 35, height: 35 }}/>,
+    'wallet': <AccountIcon style={{ width: 35, height: 35 }}/>,
     'default':<AccountIcon style={{ width: 35, height: 35 }}/>   
-    // TODO： ADD MORE
+    // TODO： Replace icon
 }
