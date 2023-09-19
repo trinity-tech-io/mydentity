0c8d614e088b7aafff7fcdd5492b3b3a402f521f
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2023-09-19 08:19:33 +0800

    Added applications menu

---
+++ client/did-web-service/src/app/(pages)/applications/page.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+import { FC } from "react";
+
+const Applications: FC = () => {
+  return (<div className="col-span-full">
+    Applications
+    <Typography>Applications this identity interacted with</Typography>
+  </div>)
+}
+
+export default Applications;
