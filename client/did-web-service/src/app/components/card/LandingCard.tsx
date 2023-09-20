294ced6ecdda122919385c4e5a075fdafa1d42e8
fullstack12345 <leo.hhu5731@gmail.com>
2023-09-19 22:07:50 -0500

    maintain aspect ratio of card component

--- client/did-web-service/src/app/components/card/LandingCard.tsx
+++ client/did-web-service/src/app/components/card/LandingCard.tsx
@@ -1,56 +1,57 @@
 import { FC } from "react";
-import { Card, Divider } from '@mui/material';
-import Barcode from 'react-barcode'
-import ChipIcon from '@assets/images/card/Chip.svg';
-import WaveLogoIcon from '@assets/images/card/WaveLogo.svg';
-import CircleVector from '@assets/images/card/Circle.svg';
+import { Card, Divider, Grid } from "@mui/material";
+import ChipIcon from "@assets/images/card/Chip.svg";
+import WaveLogoIcon from "@assets/images/card/WaveLogo.svg";
+import CircleVector from "@assets/images/card/Circle.svg";
 import TextBarcode from "@components/text-barcode/TextBarcode";
 
 export const LandingCard: FC = () => {
-  const BarCodeOptions = {
-    displayValue: false,
-    margin: 0,
-    background: '#ffffffff00',
-    lineColor: '#ffffff',
-    height: 35,
-    width: 1.3
-  }
-
   return (
-    <Card className="lg:w-[450px] sm:w-8/12 bg-black noise-bg border-white border-opacity-30 border-2 rounded-3xl relative bg-color">
-      <div className="absolute w-4/6">
-        <CircleVector width='100%' height='100%' viewBox="0 0 349 354"/>
-      </div>
-      <div className="absolute w-full circle-bottom-box">
-        <CircleVector width='100%' height='100%' viewBox="0 0 349 354"/>
-      </div>
-      <div className="px-6 pt-5 pb-14">
-        <div className="flex">
-          <div className="h-7">
-            <ChipIcon width='100%' height='100%' viewBox="0 0 50 38"/>
-          </div>
-          <div className="flex-1" />
-          <div className="h-7">
-            <WaveLogoIcon width='100%' height='100%' viewBox="0 0 32 40"/>
-          </div>
+    <Card className="card-wrapper max-xl:w-11/12 lg:w-[450px] sm:w-8/12 bg-black noise-bg border-white border-opacity-30 border-2 rounded-3xl relative bg-color">
+      <div className="body">
+        <div className="absolute w-4/6">
+          <CircleVector width="100%" height="100%" viewBox="0 0 349 354" />
         </div>
-        <div className="w-full">
-          <TextBarcode
-            value="Welcome to future"
-            text="Welcome to the future"
-            outerClassName="pt-10"
-            textClassName="text-sm tracking-[7px]"
-            height={22}
-          />
+        <div className="absolute w-full circle-bottom-box">
+          <CircleVector width="100%" height="100%" viewBox="0 0 349 354" />
         </div>
-        <TextBarcode
-          value="identity"
-          text="of identity"
-          textClassName="text-sm tracking-[9px]"
-          height={22}
-        />
-        <Divider className='border-neutral-50 border-opacity-20 pt-1'/>
+        <div className="px-6 pt-5 pb-14">
+          <Grid container spacing={{xl: 0.5, sm:0.8, md: 1}}>
+            <Grid item xs={12}>
+              <div className="flex pb-7">
+                <div className="h-6 md:h-7">
+                  <ChipIcon width="100%" height="100%" viewBox="0 0 50 38" />
+                </div>
+                <div className="flex-1" />
+                <div className="h-6 md:h-7">
+                  <WaveLogoIcon
+                    width="100%"
+                    height="100%"
+                    viewBox="0 0 32 40"
+                  />
+                </div>
+              </div>
+            </Grid>
+            <Grid item xs={12}>
+              <TextBarcode
+                value="Welcome to future"
+                text="Welcome to the future"
+                textClassName="text-sm tracking-[7px]"
+                height={22}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextBarcode
+                value="identity"
+                text="of identity"
+                textClassName="text-sm tracking-[9px]"
+                height={22}
+              />
+            </Grid>
+          </Grid>
+          <Divider className="border-neutral-50 border-opacity-20" />
+        </div>
       </div>
     </Card>
-  )
-}
+  );
+};
