d5dc73a4fde026e3c195ee505279ce60b77a972d
fullstack12345 <leo.hhu5731@gmail.com>
2023-09-21 22:14:08 -0500

    switch to password form layout when click next button in signup page (no input yet)

--- client/did-web-service/src/app/(pages)/register/page.tsx
+++ client/did-web-service/src/app/(pages)/register/page.tsx
@@ -1,19 +1,43 @@
 "use client";
-import { ChangeEventHandler, FC, useRef, useState } from "react";
+import React, { FC, useRef, useState } from "react";
+import clsx from "clsx";
 // import Xarrow from "react-xarrows";
-import {
-  Card,
-  useMediaQuery,
-  useTheme,
-  styled,
-  FormControl,
-  Input,
-  InputAdornment,
-  Fade,
-} from "@mui/material";
+import { Card, useMediaQuery, useTheme, styled, FormControl, Input, InputAdornment, Fade, Box } from "@mui/material";
 import TextBarcode from "@components/text-barcode/TextBarcode";
 import { BlackButton } from "@components/button";
 import { LandingCard } from "@components/card";
+
+const CaseWrapper = styled(Box)(({ theme }) => ({
+  minWidth: 180,
+  perspective: 600,
+  borderRadius: "1.5rem",
+  "&:after": {
+    paddingTop: "73%",
+    display: "block",
+    content: "''",
+  },
+  ".card": {
+    position: "relative",
+    width: "100%",
+    height: "100%",
+    cursor: "pointer",
+    transformStyle: "preserve-3d",
+    transformOrigin: "center right",
+    transition: "transform 0.5s",
+    ".card-face": {
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      backfaceVisibility: "hidden",
+    },
+    ".back": {
+      transform: "rotateY(180deg)",
+    },
+  },
+  ".card.is-flipped": {
+    transform: "translateX(-100%) rotateY(-180deg)",
+  },
+}));
 
 const CardCase = styled(Card)(({ theme }) => ({
   minWidth: 180,
@@ -66,11 +90,16 @@ const RegisterPage: FC = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   const [holderName, setHolderName] = useState("");
-  const [visibleNext, setVisibleNext] = useState(false);
+  const [visibleNextBtn, setVisibleNextBtn] = useState(false);
+  const [visibleNextForm, setVisibleNextForm] = useState(false);
 
-  const handleInputName: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setVisibleNext(true);
+  const handleInputName: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setVisibleNextBtn(true);
     setHolderName(e.target.value);
+  };
+
+  const handleNext: React.MouseEventHandler = () => {
+    setVisibleNextForm(true);
   };
 
   return (
@@ -99,46 +128,65 @@ const RegisterPage: FC = () => {
         />
       </div>
       <div className="w-4/5 max-w-md flex items-center flex-col m-auto">
-        <CardCase className="relative w-full md:pb-2">
-          <div className="absolute inset-0 p-2">
-            <div className="dashed-body w-full h-full rounded-2xl p-1.5">
-              <div className="flex flex-col h-full">
-                <div className="basis-[11%] overflow-hidden">
-                  <LandingCard className="w-full bg-[#523E21]" />
-                </div>
-                <div className="basis-[50%] overflow-hidden pt-2 relative">
-                  <LandingCard className="w-full bg-neutral-950" />
-                  <div className="compartment absolute bottom-0" />
-                </div>
-                <div className="basis-[39%] flex items-center">
-                  <div className="px-[10%] pb-4 w-full">
-                    <FormControlStyled fullWidth>
-                      <label
-                        htmlFor="name-adornment"
-                        className="text-white text-[10px]"
-                      >
-                        ACCOUNT NAME
-                      </label>
-                      <Input
-                        id="name-adornment"
-                        inputProps={{
-                          maxLength: 30,
-                        }}
-                        startAdornment={
-                          <InputAdornment position="start"></InputAdornment>
-                        }
-                        onChange={handleInputName}
-                      />
-                    </FormControlStyled>
+        <CaseWrapper className="wrapper relative w-full md:pb-2">
+          <div className={clsx("card", visibleNextForm && "is-flipped")}>
+            <div className="card-face front">
+              <CardCase className="relative w-full md:pb-2">
+                <div className="absolute inset-0 p-2">
+                  <div className="dashed-body w-full h-full rounded-2xl p-1.5">
+                    <div className="flex flex-col h-full">
+                      <div className="basis-[11%] overflow-hidden">
+                        <LandingCard className="w-full bg-[#523E21]" />
+                      </div>
+                      <div className="basis-[50%] overflow-hidden pt-2 relative">
+                        <LandingCard className="w-full bg-neutral-950" />
+                        <div className="compartment absolute bottom-0" />
+                      </div>
+                      <div className="basis-[39%] flex items-center">
+                        <div className="px-[10%] pb-4 w-full">
+                          <FormControlStyled fullWidth>
+                            <label
+                              htmlFor="name-adornment"
+                              className="text-white text-[10px]"
+                            >
+                              ACCOUNT NAME
+                            </label>
+                            <Input
+                              id="name-adornment"
+                              inputProps={{
+                                maxLength: 30,
+                              }}
+                              startAdornment={
+                                <InputAdornment position="start"></InputAdornment>
+                              }
+                              onChange={handleInputName}
+                            />
+                          </FormControlStyled>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
+              </CardCase>
+            </div>
+            <div className="card-face back">
+              <CardCase className="relative w-full md:pb-2">
+                <div className="absolute inset-0 p-2">
+                  <div className="dashed-body w-full h-full rounded-2xl p-1.5"></div>
+                </div>
+              </CardCase>
             </div>
           </div>
-        </CardCase>
+        </CaseWrapper>
         <div className="p-8 w-full">
-          <Fade in={visibleNext}>
-            <BlackButton className="w-full" disabled={!holderName.length}>NEXT</BlackButton>
+          <Fade in={visibleNextBtn}>
+            <BlackButton
+              className="w-full"
+              disabled={!holderName.length}
+              onClick={handleNext}
+            >
+              {visibleNextForm ? "CREATE ACCOUNT" : "NEXT"}
+            </BlackButton>
           </Fade>
         </div>
       </div>
