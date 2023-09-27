216f32452ee66fc799029ef0f037c57203303d33
yufei <chentj070921@yeah.net>
2023-09-27 11:20:28 +0800

    Clean on auth-provider module.

--- server/api/src/auth-providers/auth-providers.controller.ts
+++ server/api/src/auth-providers/auth-providers.controller.ts
@@ -1,23 +1,8 @@
-import { Controller, Get, Redirect, Req, UseGuards } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { AppException } from "../exceptions/app-exception";
-import { AuthExceptionCode } from "../exceptions/exception-codes";
-import { logger } from "../logger";
-import { UserService } from "../user/user.service";
-import { MicrosoftProfileService } from "./microsoft-profile.service";
 
 @Controller()
 export class AuthProvidersController {
-  private static readonly ACTION_BIND = 'bind';
-  private static readonly ACTION_LOGIN = 'login';
-
-  constructor(
-    private readonly configService: ConfigService,
-    private readonly microsoftProfileService: MicrosoftProfileService,
-    private readonly userService: UserService
-  ) { }
-
   /**
    * Start oauth from server.
    * @param req
