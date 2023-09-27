6ac08233afc6d5decf8b54cd10ac3ad93b52e916
yufei <chentj070921@yeah.net>
2023-09-27 18:28:35 +0800

    Update after rebase.

--- client/did-web-service/src/app/components/security/bindemail/widgets/BindEmailWithGoogle.tsx
+++ client/did-web-service/src/app/components/security/bindemail/widgets/BindEmailWithGoogle.tsx
@@ -1,40 +1,27 @@
 'use client';
-import { MainButton } from '@components/generic/MainButton';
 import { Icon as ReactIcon } from '@iconify/react';
-import { makeStyles } from '@mui/styles';
 import { FlowOperation, setOnGoingFlowOperation } from "@services/flow.service";
-import clsx from 'clsx';
 import { useRouter } from "next/navigation";
 import { FC } from 'react';
-
-const useStyles = makeStyles((theme) => ({
-  centeredContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    minHeight: '6vh',
-  },
-}));
+import { DarkButton } from "@components/button";
 
 const BindEmailWithGoogle: FC = () => {
-  const classes = useStyles();
   const router = useRouter();
 
-  const signInWithGoogle = (): void => {
+  const signInWithMicrosoft = (): void => {
     setOnGoingFlowOperation(FlowOperation.OnBoardingEmailBinding);
     router.push(`${process.env.NEXT_PUBLIC_BACKEND_URL}/google`);
   };
 
   return (
-    <div className={clsx(classes.centeredContainer, 'relative')}>
-      <MainButton
-        leftIcon={<ReactIcon icon="logos:google-icon" />}
-        onClick={signInWithGoogle}
+      <DarkButton
+          id="bind-ms"
+          startIcon={<ReactIcon icon="logos:google-icon" />}
+          onClick={signInWithMicrosoft}
+          className="w-full"
       >
         Bind email with Google
-      </MainButton>
-    </div>
+      </DarkButton>
   );
 };
 
