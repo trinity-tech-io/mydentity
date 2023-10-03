67d601c8db1c9cab16e7dbe6ccd302834540247a
fullstack12345 <leo.hhu5731@gmail.com>
2023-10-03 05:22:08 -0500

    update Recent Activity component

--- client/did-web-service/src/app/(pages)/dashboard/widgets/recent-activity/RecentActivity.tsx
+++ client/did-web-service/src/app/(pages)/dashboard/widgets/recent-activity/RecentActivity.tsx
@@ -1,11 +1,12 @@
+import { FC } from "react";
+import { Avatar, ListItemText, TableCell, styled } from "@mui/material";
+import { Icon as ReactIcon } from "@iconify/react";
 import { useBehaviorSubject } from "@hooks/useBehaviorSubject";
 import { authUser$ } from "@services/user/user.events";
 import { useRouter } from "next/navigation";
-import { FC } from 'react';
 import { RecentActivityRow } from "./RecentActivityRow";
 import DetailContainer from "@components/generic/DetailContainer";
-import { Avatar, Divider, ListItemText, Stack, Typography, styled } from "@mui/material";
-import { Icon as ReactIcon } from "@iconify/react";
+import { DetailTable, DetailTableRow } from "@components/generic/DetailTable";
 
 const IconAvatar = styled(Avatar)(({ theme }) => ({
   backgroundColor: "#3A3A3A",
@@ -15,39 +16,56 @@ const IconAvatar = styled(Avatar)(({ theme }) => ({
   padding: 8,
 }));
 
-export const RecentActivityWidget: FC = _ => {
+export const RecentActivityWidget: FC = (_) => {
   const [activeUser] = useBehaviorSubject(authUser$);
   const router = useRouter();
-  let [activities] = useBehaviorSubject(activeUser?.get('activity').activities$);
-  activities = activities?.slice(0, 5)
+  let [activities] = useBehaviorSubject(
+    activeUser?.get("activity").activities$
+  );
+  activities = activities?.slice(0, 5);
 
   const handleShowAllClick = (): void => {
     router.push("/recent-activity");
-  }
+  };
 
   return (
     <DetailContainer title="Recent Activity" showAllAction={handleShowAllClick}>
       <div className="mb-1">
-        <Stack direction="row" className="pl-12 pr-2 py-1" sx={{background: "linear-gradient(to right, transparent, #444 25%, #3e3e3e 50%, #444 75%, transparent)"}}>
-          <span className="text-[15px] flex-1 font-medium">ACCOUNT ACTIVITY</span>
-          <span className="text-[15px] font-medium">DATE</span>
-        </Stack>
-        <Stack direction="row" spacing={1}>
-          <div className="inline-flex items-center">
-            <IconAvatar ><ReactIcon icon="ic:round-log-in" /></IconAvatar>
-          </div>
-          <div className="flex-1">
-            <div className="flex flex-1 items-center pl-1">
-              <ListItemText
-                className="flex-1"
-                primary={<span className="font-medium">User sign-in</span>}
-                secondary={<span className="text-[12px]">Microsoft sign-in</span>}
+        <DetailTable
+          headCells={
+            <>
+              <TableCell>ACCOUNT ACTIVITY</TableCell>
+              <TableCell>DATE</TableCell>
+            </>
+          }
+          bodyRows={
+            <>
+              <DetailTableRow
+                avatar={
+                  <IconAvatar>
+                    <ReactIcon icon="ic:round-log-in" />
+                  </IconAvatar>
+                }
+                rowCells={
+                  <>
+                    <TableCell>
+                      <ListItemText
+                        className="flex-1"
+                        primary={
+                          <span className="font-medium">User sign-in</span>
+                        }
+                        secondary={
+                          <span className="text-[12px]">Microsoft sign-in</span>
+                        }
+                      />
+                    </TableCell>
+                    <TableCell>1 min ago</TableCell>
+                  </>
+                }
               />
-              <span>1 min ago</span>
-            </div>
-            <Divider />
-          </div>
-        </Stack>
+            </>
+          }
+        />
       </div>
       {/* Table */}
       <div className="overflow-x-hidden">
@@ -65,9 +83,11 @@ export const RecentActivityWidget: FC = _ => {
           </thead>
           {/* Table body */}
           <tbody className="text-sm divide-y divide-slate-100 dark:divide-slate-700">
-            {
-              activities && activities.length > 0 && activities.map((activity, i) => <RecentActivityRow activity={activity} key={i} />)
-            }
+            {activities &&
+              activities.length > 0 &&
+              activities.map((activity, i) => (
+                <RecentActivityRow activity={activity} key={i} />
+              ))}
           </tbody>
         </table>
       </div>
@@ -81,4 +101,4 @@ export const RecentActivityWidget: FC = _ => {
     //   </div>
     // </div>
   );
-}
+};
---
+++ client/did-web-service/src/app/components/generic/DetailTable.tsx
@@ -0,0 +1,48 @@
+import { FC, ReactNode } from "react";
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+
+export const DetailTableRow: FC<{ avatar: ReactNode; rowCells: ReactNode }> = ({
+  avatar,
+  rowCells,
+}) => {
+  return (
+    <TableRow
+    // sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+    >
+      <TableCell padding="none">{avatar}</TableCell>
+      {rowCells}
+    </TableRow>
+  );
+};
+export const DetailTable: FC<{ headCells: ReactNode; bodyRows: ReactNode }> = ({
+  headCells,
+  bodyRows,
+}) => {
+  return (
+    <Table
+      size="small"
+      sx={{
+        th: { border: 0 },
+        thead: {
+          background:
+            "linear-gradient(to right, transparent, #444 25%, #3e3e3e 50%, #444 75%, transparent)",
+          th: { px: 1, fontSize: 15, fontWeight: "600" },
+          "th:last-child": { textAlign: "right" },
+        },
+        tbody: {
+          td: { px: 1, py: 0 },
+          "td:first-child": { border: 0, pl: 0, pr: 1 },
+          "td:last-child": { textAlign: "right" },
+        },
+      }}
+    >
+      <TableHead>
+        <TableRow>
+          <TableCell sx={{ width: 0 }} />
+          {headCells}
+        </TableRow>
+      </TableHead>
+      <TableBody>{bodyRows}</TableBody>
+    </Table>
+  );
+};
