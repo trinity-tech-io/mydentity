e388485179bde1d6feb89cc80956b6f5934d872f
fullstack12345 <leo.hhu5731@gmail.com>
2023-10-02 21:14:37 -0500

    update Headline component for dashboard page

--- client/did-web-service/src/app/components/layout/Headline.tsx
+++ client/did-web-service/src/app/components/layout/Headline.tsx
@@ -1,13 +1,21 @@
+import { styled } from "@mui/material";
 import { FC, ReactNode } from "react";
 
-const Headline: FC<{ title: string, description: string | ReactNode }> = ({ title, description }) => (
-    <>
-        <h3 className='w-full text-4xl font-bold'>
-            {title}
-        </h3>
-        <p className="mt-4">
-            {description}
-        </p>
-    </>
-)
-export default Headline
+const HeadlineBox = styled("div")((props: { showBg: boolean }) => {
+  const { showBg } = props;
+  return {
+    marginBottom: "1.5rem",
+    background: showBg ? "url('./headline-banner.png') no-repeat center center / cover" : "none",
+  };
+});
+const Headline: FC<{
+  title: string;
+  description: string | ReactNode;
+  showBg?: boolean;
+}> = ({ title, description, showBg = false }) => (
+  <HeadlineBox showBg={showBg} className={showBg?"p-6 rounded-lg":""}>
+    <h3 className="w-full text-4xl font-bold pb-2">{title}</h3>
+    <p className="mt-4">{description}</p>
+  </HeadlineBox>
+);
+export default Headline;
