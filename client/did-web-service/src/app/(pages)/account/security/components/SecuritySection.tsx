4f14d3a55c7c64c0b66af8157222dd2b5fc0a9e7
fullstack12345 <leo.hhu5731@gmail.com>
2023-10-09 02:01:25 -0500

    update sections in security center page

---
+++ client/did-web-service/src/app/(pages)/account/security/components/SecuritySection.tsx
@@ -0,0 +1,56 @@
+import { FC, ReactNode } from "react";
+import clsx from "clsx";
+import { Box, Card, CardContent, styled } from "@mui/material";
+import { DarkButton } from "@components/button";
+import { IconAvatar } from "@components/feature/DetailLine";
+
+const CardStyled = styled(Card)(({ theme }) => ({
+  border: "1px solid #FFFFFF55",
+  borderRadius: "0.5rem",
+  background: "#1D1D1D",
+  position: "relative",
+}));
+const SecuritySection: FC<{
+  title: string | ReactNode;
+  icon: ReactNode;
+  className?: string;
+  children: ReactNode;
+  isSet: boolean;
+  statusTitle: string;
+  actionTitle: string;
+  handleAction?: () => void;
+}> = ({
+  title,
+  icon,
+  children,
+  className = "",
+  isSet,
+  statusTitle,
+  actionTitle,
+  handleAction = (): void => {},
+}) => {
+  return (
+    <CardStyled className={clsx("border bord", className)} elevation={0}>
+      <CardContent className="relative z-10 flex flex-col h-full" sx={{ px: 3, pt: 1 }}>
+        <Box className="pb-4 pt-2 flex items-center">
+          <IconAvatar>{icon}</IconAvatar>
+          <span className="flex-1 text-[20px] font-semibold ml-2">{title}</span>
+          <Box
+            className={clsx(
+              "rounded-md text-[7pt] px-3 py-0.5 inline-block text-white whitespace-nowrap",
+              isSet ? "bg-[#34A853]" : "bg-[#EA4335]"
+            )}
+          >
+            {statusTitle}
+          </Box>
+        </Box>
+        <div className="flex-1 pb-[5%]">
+          {children}
+        </div>
+        <DarkButton onClick={handleAction}>{actionTitle}</DarkButton>
+      </CardContent>
+    </CardStyled>
+  );
+};
+
+export default SecuritySection;
--- client/did-web-service/src/app/(pages)/account/security/page.tsx
+++ client/did-web-service/src/app/(pages)/account/security/page.tsx
@@ -1,17 +1,20 @@
 "use client";
-import EmailIcon from '@assets/images/email.svg';
-import FingerprintIcon from '@assets/images/fingerprint.svg';
-import PasswordIcon from '@assets/images/password.svg';
-import { Breadcrumbs } from '@components/breadcrumbs/Breadcrumbs';
+import { FC, useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Grid, Typography } from "@mui/material";
+import { Icon as ReactIcon } from "@iconify/react"
+import EmailIcon from "@assets/images/email.svg";
+import FingerprintIcon from "@assets/images/fingerprint.svg";
+import PasswordIcon from "@assets/images/password.svg";
+import { Breadcrumbs } from "@components/breadcrumbs/Breadcrumbs";
 import { MainButton } from "@components/generic/MainButton";
 import { useBehaviorSubject } from "@hooks/useBehaviorSubject";
 import { useMounted } from "@hooks/useMounted";
-import { Typography } from "@mui/material";
 import { authUser$ } from "@services/user/user.events";
-import Link from 'next/link';
-import { useRouter, useSearchParams } from "next/navigation";
-import { FC, useEffect, useState } from "react";
-import { BrowserRow } from './components/BrowserRow';
+import { BrowserRow } from "./components/BrowserRow";
+import Headline from "@components/layout/Headline";
+import SecuritySection from "./components/SecuritySection";
 
 const Security: FC = () => {
   const { mounted } = useMounted();
@@ -28,17 +31,17 @@ const Security: FC = () => {
   const router = useRouter();
 
   const searchParams = useSearchParams();
-  const error = searchParams.get('error');
+  const error = searchParams.get("error");
   const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
-    if (error && error !== '') {
+    if (error && error !== "") {
       switch (error) {
-        case 'emailExists':
-          setErrorMsg('Email already exists');
+        case "emailExists":
+          setErrorMsg("Email already exists");
           break;
         default:
-          setErrorMsg('Unknown error, please try again.');
+          setErrorMsg("Unknown error, please try again.");
           break;
       }
     }
@@ -46,119 +49,178 @@ const Security: FC = () => {
 
   const bindDevice = (): void => {
     // securityFeature.bindDevice();
-  }
+  };
 
   const bindPasskey = (): void => {
     router.push("/account/security/bind-passkey");
-  }
+  };
 
   const bindPassword = (): void => {
     router.push("/account/security/bind-password");
-  }
+  };
 
   const bindEmail = (): void => {
     router.push("/account/security/bind-email");
-  }
+  };
 
-  return (<div className="col-span-full">
-    <Breadcrumbs entries={["security-center"]} />
-    <Typography variant="h4">Security center</Typography>
-    <p>
-      Your identity is a Web3 identity, <b>protected by cryptographic keys</b>. Many Web3 applications require you to
-      save those keys by yourself, and you will sometimes do that in unsafe ways. On the contrary, this service
-      partially stores the complex cryptographic keys
-      for you so you don&apos;t have to do it. Your keys are protected by your own browsers or passwords
-      and <b>this app cannot do anything without your consent</b>. For this reason, you need to bind multiple devices and browsers,
-      as this is your only way to recover your account later in case one of the devices is lost. <b>We cannot do that for you</b>.
-    </p>
-    <br /><br />
-    {mounted && <>
-      <div className='grid grid-cols-12 items-start gap-10'>
-        <div className='col-span-full xl:col-span-6 flex flex-row gap-10 items-start'>
-          <EmailIcon width={60} />
-          {
-            !isEmailBound &&
-            <div className="flex flex-col flex-1">
-              <div className="info mb-2">
-                Attaching your <b>email address</b> to your account allows you to sign in later. Without email,
-                you will be able to sign in using browser biometrics if configured.
-              </div>
-              <MainButton onClick={bindEmail} className="self-start">Verify your email address</MainButton>
-              {errorMsg && <>
-                <div className='text-red-500'>{errorMsg}</div>
-              </>
-              }
+  return (
+    <div className="col-span-full">
+      {/* <Breadcrumbs entries={["security-center"]} /> */}
+      <Headline
+        title="Security Center"
+        description="Many Web3 apps require you to manage complex cryptographic keys, which can sometimes lead to unsafe practices. Our service securely stores some keys
+      for you, ensuring your control and security. Link multiple devices for future account recovery."
+        showBg={true}
+      />
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <SecuritySection
+            className="h-full"
+            icon={<ReactIcon icon="entypo:email" />}
+            title="Connect email address"
+            statusTitle="EMAIL NOT LINKED"
+            isSet={false}
+            actionTitle="VERIFY EMAIL"
+            handleAction={bindEmail}
+          >
+            <Typography variant="body2">Connecting your email to your account allows you to log in later. If you haven't linked an email, you can still log in using browser biometrics if it's set up.</Typography>
+          </SecuritySection>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <SecuritySection
+            className="h-full"
+            icon={<ReactIcon icon="ic:round-password" />}
+            title="Set master password"
+            statusTitle="PASSWORD SET"
+            isSet={true}
+            actionTitle="UPDATE PASSWORD"
+            handleAction={bindPassword}
+          >
+            <Typography variant="body2">You've successfully set up your master password, which is like a key to your account's security. This important step helps keep your account safe and under your control.</Typography>
+          </SecuritySection>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <SecuritySection
+            icon={<ReactIcon icon="fluent-mdl2:website" />}
+            title="Link browser via biometric passkey"
+            statusTitle="BROWSER NOT BOUND"
+            isSet={false}
+            actionTitle="SECURE BIOMETRICS"
+            handleAction={bindPasskey}
+          >
+            <Typography variant="body2">When you link your account to your browser's biometrics, you can only access this app from that specific browser. It also lets you unlock the encryption key, keeping your data safe from unauthorized access, even potential attackers.</Typography>
+          </SecuritySection>
+        </Grid>
+      </Grid>
+      {mounted && (
+        <>
+          <div className="grid grid-cols-12 items-start gap-10">
+            <div className="col-span-full xl:col-span-6 flex flex-row gap-10 items-start">
+              <EmailIcon width={60} />
+              {!isEmailBound && (
+                <div className="flex flex-col flex-1">
+                  <div className="info mb-2">
+                    Attaching your <b>email address</b> to your account allows
+                    you to sign in later. Without email, you will be able to
+                    sign in using browser biometrics if configured.
+                  </div>
+                  <MainButton onClick={bindEmail} className="self-start">
+                    Verify your email address
+                  </MainButton>
+                  {errorMsg && (
+                    <>
+                      <div className="text-red-500">{errorMsg}</div>
+                    </>
+                  )}
+                </div>
+              )}
+              {isEmailBound && (
+                <div className="flex flex-col flex-1">
+                  <div>Email addresses already bound</div>
+                  <div>
+                    {userEmails.map((email) => (
+                      <div key={email.id} className="info mb-2">
+                        {email.email}
+                      </div>
+                    ))}
+                  </div>
+                  <Link href="/account/security/bind-email">Bind more</Link>
+                  {errorMsg && (
+                    <>
+                      <div className="text-red-500">{errorMsg}</div>
+                    </>
+                  )}
+                </div>
+              )}
             </div>
-          }
-          {
-            isEmailBound && <div className='flex flex-col flex-1'>
-              <div>Email addresses already bound</div>
-              <div>
-                {userEmails.map(email => <div key={email.id} className="info mb-2">{email.email}</div>)}
-              </div>
-              <Link href="/account/security/bind-email">Bind more</Link>
-              {errorMsg && <>
-                <div className='text-red-500'>{errorMsg}</div>
-              </>
-              }
-            </div>
-          }
-        </div>
 
-        {/* Password */}
-        <div className='col-span-full xl:col-span-6 flex flex-row gap-10 items-start'>
-          <PasswordIcon width={60} />
-          {
-            !isPasswordBound &&
-            <div className="flex flex-col flex-1">
-              <div className="info mb-2">
-                By defining <b>master password</b>, all your personal information stored in our service gets encrypted and can only
-                be accessed with your approval. Note that this password can only changed if you have another encryption method
-                defined, such as the browser biometrics.
-              </div>
-              <MainButton onClick={bindPassword} className="self-start">Bind a master password</MainButton>
+            {/* Password */}
+            <div className="col-span-full xl:col-span-6 flex flex-row gap-10 items-start">
+              <PasswordIcon width={60} />
+              {!isPasswordBound && (
+                <div className="flex flex-col flex-1">
+                  <div className="info mb-2">
+                    By defining <b>master password</b>, all your personal
+                    information stored in our service gets encrypted and can
+                    only be accessed with your approval. Note that this password
+                    can only changed if you have another encryption method
+                    defined, such as the browser biometrics.
+                  </div>
+                  <MainButton onClick={bindPassword} className="self-start">
+                    Bind a master password
+                  </MainButton>
+                </div>
+              )}
+              {isPasswordBound && (
+                <div className="flex flex-col flex-1">
+                  <div>Your master password has been set</div>
+                  <Link href="/account/security/bind-password">
+                    Update password
+                  </Link>
+                </div>
+              )}
             </div>
-          }
-          {
-            isPasswordBound && <div className='flex flex-col flex-1'>
-              <div>Your master password has been set</div>
-              <Link href="/account/security/bind-password">Update password</Link>
-            </div>
-          }
-        </div>
 
-        {/* Passkey */}
-        <div className='col-span-full xl:col-span-6 flex flex-row gap-10 items-start'>
-          <FingerprintIcon width={60} />
-          {
-            !isThisBrowserBound &&
-            <div className="flex flex-col flex-1">
-              <div className="info mb-2">
-                Binding your account to your <b>browser biometrics</b> allows you to sign in to this app (only from this browser)
-                but also to unlock the secret key that decrypts all the data you store on our servers. Without this key, we,
-                or potential attackers, are not able to read your personal information in clear text.
-              </div>
-              <MainButton onClick={bindPasskey} className="self-start">Secure with this browser biometrics</MainButton>
+            {/* Passkey */}
+            <div className="col-span-full xl:col-span-6 flex flex-row gap-10 items-start">
+              <FingerprintIcon width={60} />
+              {!isThisBrowserBound && (
+                <div className="flex flex-col flex-1">
+                  <div className="info mb-2">
+                    Binding your account to your <b>browser biometrics</b>{" "}
+                    allows you to sign in to this app (only from this browser)
+                    but also to unlock the secret key that decrypts all the data
+                    you store on our servers. Without this key, we, or potential
+                    attackers, are not able to read your personal information in
+                    clear text.
+                  </div>
+                  <MainButton onClick={bindPasskey} className="self-start">
+                    Secure with this browser biometrics
+                  </MainButton>
+                </div>
+              )}
+              {isThisBrowserBound && (
+                <div className="flex flex-col flex-1">
+                  <div>Your browser is bound to your account</div>
+                  <Link href="/account/security/bind-passkey">Bind again</Link>
+                </div>
+              )}
             </div>
-          }
-          {
-            isThisBrowserBound && <div className='flex flex-col flex-1'>
-              <div>Your browser is bound to your account</div>
-              <Link href="/account/security/bind-passkey">Bind again</Link>
-            </div>
-          }
-        </div>
-      </div>
+          </div>
 
-      <br /><br />
+          <br />
+          <br />
 
-      <Typography variant="h5">My browsers</Typography>
-      {browsers?.length == 0 && "No browser used so far."}
-      {
-        browsers && browsers.map((browser, i) => <BrowserRow key={i} browser={browser} />)
-      }
-    </>}
-  </div>)
-}
+          <Typography variant="h5">My browsers</Typography>
+          {browsers?.length == 0 && "No browser used so far."}
+          {browsers &&
+            browsers.map((browser, i) => (
+              <BrowserRow key={i} browser={browser} />
+            ))}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Security;
