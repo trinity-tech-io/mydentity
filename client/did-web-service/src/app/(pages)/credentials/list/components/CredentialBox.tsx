f82fed8989c450aee99569e324647299401df3a5
fullstack12345 <leo.hhu5731@gmail.com>
2023-10-16 19:39:24 -0700

    display shared app count badge in credential grid box

--- client/did-web-service/src/app/(pages)/credentials/list/components/CredentialBox.tsx
+++ client/did-web-service/src/app/(pages)/credentials/list/components/CredentialBox.tsx
@@ -5,47 +5,68 @@ import { CardStyled } from "@/app/(pages)/account/security/components/SecuritySe
 import { Credential } from "@model/credential/credential";
 import { CredentialAvatar } from "@components/credential/CredentialAvatar";
 import { JsonViewer } from "@components/credential/JsonViewer";
+import { useBehaviorSubject } from "@hooks/useBehaviorSubject";
+import SharedCountLabel from "@components/credential/SharedCountLabel";
 
 const CredentialBox: FC<{ credential: Credential }> = ({ credential }) => {
+  const [requestingApplications] = useBehaviorSubject(
+    credential?.requestingApplications$
+  );
+  const [isConform] = useBehaviorSubject(credential?.isConform$);
+
   return (
-    <CardStyled
+    <div className="relative h-full">
+      <CardStyled
         className="h-full"
-      elevation={0}
-      sx={{ px: "12px", py: "10px", display: "grid", verticalAlign: "middle" }}
-    >
-      <Stack direction="row" alignItems="center" overflow="hidden">
-        <Stack
-          direction="row"
-          spacing={1}
-          flexGrow={1}
-          alignItems="center"
-          overflow="hidden"
-        >
-          <CredentialAvatar credential={credential} width={32} height={32} />
-          <Stack overflow="hidden">
-            <Typography variant="body2" fontWeight={600} noWrap={true}>
-              {credential.getDisplayableTitle()}
-            </Typography>
-            <Typography variant="caption" fontSize="9pt" noWrap={true}>
-              {credential.getDisplayValue() && (
-                <JsonViewer data={credential.getDisplayValue()} />
-              )}
-            </Typography>
-          </Stack>
-        </Stack>
-        <div>
-          <IconButton
-            size="small"
-            color="inherit"
-            // onClick={(event): void => {
-            //   handleOpenMenu(event, credential);
-            // }}
+        elevation={0}
+        sx={{
+          pl: "12px",
+          pr: "4px",
+          py: "10px",
+          display: "grid",
+          verticalAlign: "middle",
+        }}
+      >
+        <Stack direction="row" alignItems="center" overflow="hidden">
+          <Stack
+            direction="row"
+            spacing={1}
+            flexGrow={1}
+            alignItems="center"
+            overflow="hidden"
           >
-            <MoreVertIcon />
-          </IconButton>
-        </div>
-      </Stack>
-    </CardStyled>
+            <CredentialAvatar credential={credential} width={32} height={32} />
+            <Stack overflow="hidden">
+              <Typography variant="body2" fontWeight={600} noWrap={true}>
+                {credential.getDisplayableTitle()}
+              </Typography>
+              <Typography variant="caption" fontSize="9pt" noWrap={true}>
+                {credential.getDisplayValue() && (
+                  <JsonViewer data={credential.getDisplayValue()} />
+                )}
+              </Typography>
+            </Stack>
+          </Stack>
+          <div>
+            <IconButton
+              size="small"
+              color="inherit"
+              // onClick={(event): void => {
+              //   handleOpenMenu(event, credential);
+              // }}
+            >
+              <MoreVertIcon fontSize="small"/>
+            </IconButton>
+          </div>
+        </Stack>
+      </CardStyled>
+      <div className="inline-flex absolute bottom-0 right-0 translate-x-1/3 translate-y-1/3">
+        <SharedCountLabel
+          count={requestingApplications?.length || 0}
+          isConform={isConform}
+        />
+      </div>
+    </div>
   );
 };
 export default CredentialBox;
