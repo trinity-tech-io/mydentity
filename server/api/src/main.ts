2b4bc6ba451af8b888e7e27709f7de4d0592a63c
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2023-10-19 10:34:48 +0800

    Fixed api starting with node https server in staging

--- server/api/src/main.ts
+++ server/api/src/main.ts
@@ -6,15 +6,15 @@ import { KeyRingService } from './key-ring/key-ring.service';
 async function bootstrap() {
   await KeyRingService.init();
 
-  const httpsOptions = {
-    key: readFileSync('./certificates/dev-server.key'),
-    cert: readFileSync('./certificates/dev-server.crt'),
-  };
-
-  const useHttps = process.env.SERVER_URL.startsWith("https");
+  const useHttps = process.env.START_AS_NODE_HTTPS_SERVER === "true";
 
   const app = await NestFactory.create(AppModule, {
-    ...(useHttps && { httpsOptions }),
+    ...(useHttps && {
+      httpsOptions: {
+        key: readFileSync('./certificates/dev-server.key'),
+        cert: readFileSync('./certificates/dev-server.crt'),
+      }
+    }),
   });
   app.enableCors();
   await app.listen(3000);
