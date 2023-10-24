591df4a391da24424ccc6b27c8cbea828dc2d160
yufei <chentj070921@yeah.net>
2023-10-24 10:02:15 +0800

    Production build

--- deployment/deploy-to-server.sh
+++ deployment/deploy-to-server.sh
@@ -1,5 +1,5 @@
 echo "LOCAL EXECUTION"
-echo ""
+echo "Usage example(build client staging): $0 -s"
 
 # Usage is from args parse lines
 usage() { echo "$0 usage:" && grep " .)\ #" $0; exit 0; }
@@ -7,23 +7,35 @@ usage() { echo "$0 usage:" && grep " .)\ #" $0; exit 0; }
 # If no args print usage
 [ $# -eq 0 ] && usage
 
-log_tag='[deploy-to-server.sh] '
-build_client=false
-build_tests=false
-do_deploy=false
+log_tag='>>>>>> [deploy-to-server.sh] '
+build_client=false # prod
+build_tests=false # prod
+build_client_staging=false
+do_deploy_client_staging=false
+do_deploy_client_prod=false
+do_deploy_tests_prod=false
 commit_push=false
 remote_executiion=false
 
-while getopts "ctdprh" flag; do
+while getopts "cstdefprh" flag; do
     case ${flag} in
-        c) # Build the client
+        c) # Build the client prod
           build_client=true
           ;;
-        t) # Build the tests
+        s) # Build the client staging
+          build_client_staging=true
+          ;;
+        t) # Build the tests prod
           build_tests=true
           ;;
-        d) # Deploy tars of app and tests to prod.
-          do_deploy=true
+        d) # Deploy client_staging.tar.gz to vps.
+          do_deploy_client_staging=true
+          ;;
+        e) # Deploy client_prod.tar.gz to vps.
+          do_deploy_client_prod=true
+          ;;
+        f) # Deploy test_prod.tar.gz to vps.
+          do_deploy_tests_prod=true
           ;;
         p) # Commit and push
           commit_push=true
@@ -37,8 +49,12 @@ while getopts "ctdprh" flag; do
     esac
 done
 
+#########################################################################################
 # echo $build_client $build_tests $commit_push $remote_executiion
 # exit 0;
+#########################################################################################
+
+ROOT_DIR=`pwd`
 
 # Build the client dist locally
 if [ $build_client = true ]
@@ -48,6 +64,19 @@ then
   cd client/did-web-service
   npm i --legacy-peer-deps
   npm run build
+  cd ../..
+else
+  echo "${log_tag} Not building client locally as requested"
+fi
+
+# Build the client staging dist locally
+if [ $build_client_staging = true ]
+then
+  echo "${log_tag} Building client staging app locally"
+
+  cd client/did-web-service
+  npm i --legacy-peer-deps
+  npm run build:staging
   cd ../..
 else
   echo "${log_tag} Not building client locally as requested"
@@ -70,35 +99,54 @@ else
   echo "${log_tag} Not building tests locally as requested"
 fi
 
-# Deploy app and tests tar files to prod.
-if [ $do_deploy = true ]
+# Usage: deploy_tar <source_folder> <tar_file_name>
+#   ex: deploy_tar "client/did-web-service" client_staging
+function deploy_tar() {
+  source_folder=$1
+  tar_file_name=$2
+
+  echo "${log_tag} Deploy ${tar_file_name}.tar.gz to vps."
+
+  rm -f ${tar_file_name}.tar.gz
+
+  cd ${ROOT_DIR}/${source_folder}
+  tar zcvf ${tar_file_name}.tar.gz out
+
+  scp ${tar_file_name}.tar.gz did-web-service:~/
+
+  rm -f ${tar_file_name}.tar.gz
+}
+
+if [ $do_deploy_client_staging = true ]
 then
-  echo "${log_tag} Deploy app and tests tar files to prod."
-
-  # rm -f app.tar.gz tests.tar.gz
-
-  cd client/did-web-service
-  tar zcvf ../../app.tar.gz out
-  cd ../..
-
-  cd tests/did-web-tests
-  tar zcvf ../../tests.tar.gz out
-  cd ../..
-
-  # scp app.tar.gz did-web-service:~/
-  # scp tests.tar.gz did-web-service:~/
-  # rm -f app.tar.gz tests.tar.gz
+  deploy_tar client/did-web-service client-staging
 else
-  echo "${log_tag} Not deploy app and tests tar files to prod. as requested."
+  echo "${log_tag} Not client-staging.tar.gz to vps."
+fi
+
+if [ $do_deploy_client_prod = true ]
+then
+  deploy_tar client/did-web-service client-prod
+else
+  echo "${log_tag} Not client-prod.tar.gz to vps."
+fi
+
+if [ $do_deploy_tests_prod = true ]
+then
+  deploy_tar tests/did-web-tests test-prod
+else
+  echo "${log_tag} Not test-prod.tar.gz to vps."
 fi
 
 # Push git folder to git
 if [ $commit_push = true ]
 then
   echo "${log_tag} Pushing built clients and tests to git"
+
+  cd ${ROOT_DIR}
   git add .
   git commit -m "Production build"
-  git push
+  # git push
 else
   echo "${log_tag} Not commit and push to git"
 fi
@@ -106,6 +154,7 @@ fi
 # Execute remote deployment
 if [ $remote_executiion = true ]
 then
+  cd ${ROOT_DIR}
   ssh -t did-web-service 'sudo -s && bash -s' < deployment/deploy.sh
 else
   echo "${log_tag} Not execute remote deployment"
