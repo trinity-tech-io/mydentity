5b4ce50bb98ff52a954d3fe3096458c57c1e29af
fullstack12345 <leo.hhu5731@gmail.com>
2023-11-06 20:47:08 -0800

    update faq page

--- client/did-web-service/src/app/(pages)/support/faq/FAQ.tsx
+++ client/did-web-service/src/app/(pages)/support/faq/FAQ.tsx
@@ -1,52 +1,61 @@
-import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
-import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
-import MuiAccordionDetails from '@mui/material/AccordionDetails';
-import MuiAccordionSummary, { AccordionSummaryProps, } from '@mui/material/AccordionSummary';
-import Typography from '@mui/material/Typography';
-import { styled } from '@mui/material/styles';
-import { FC, useState } from 'react';
-import { FAQGroup } from './faq-types';
+import { FC, useState } from "react";
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import MuiAccordion, { AccordionProps } from "@mui/material/Accordion";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import MuiAccordionSummary, {
+  AccordionSummaryProps,
+} from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+import CardIcon from "@assets/images/card/card.svg";
+import { FAQGroup } from "./faq-types";
+import { IconAvatar } from "@components/feature/DetailLine";
 
+const AccordionWrapper = styled("div")(({ theme }) => ({
+  "&:first-child": {
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+  },
+}));
 const Accordion = styled((props: AccordionProps) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
 ))(({ theme }) => ({
   border: `1px solid ${theme.palette.divider}`,
-  '&:not(:last-child)': {
+  "&:not(:last-child)": {
     borderBottom: 0,
   },
-  '&:before': {
-    display: 'none',
+  "&:before": {
+    display: "none",
   },
 }));
 
 const AccordionSummary = styled((props: AccordionSummaryProps) => (
   <MuiAccordionSummary
-    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
     {...props}
   />
 ))(({ theme }) => ({
   backgroundColor:
-    theme.palette.mode === 'dark'
-      ? 'rgba(255, 255, 255, .05)'
-      : 'rgba(0, 0, 0, .03)',
-  flexDirection: 'row-reverse',
-  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
-    transform: 'rotate(90deg)',
+    theme.palette.mode === "dark"
+      ? "rgba(255, 255, 255, .05)"
+      : "rgba(0, 0, 0, .03)",
+  flexDirection: "row-reverse",
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
   },
-  '& .MuiAccordionSummary-content': {
+  "& .MuiAccordionSummary-content": {
     marginLeft: theme.spacing(1),
   },
 }));
 
 const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
   padding: theme.spacing(2),
-  borderTop: '1px solid rgba(0, 0, 0, .125)',
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
 }));
 
 export const FAQ: FC<{
   group: FAQGroup;
-  className?: any;
-}> = ({ group, className }) => {
+}> = ({ group }) => {
   const [expanded, setExpanded] = useState<string | false>(false);
 
   const handleChange =
@@ -55,20 +64,30 @@ export const FAQ: FC<{
     };
 
   return (
-    <div className={className}>
-      <Typography variant='h4'>
-        {group.title}
-      </Typography>
-      {group.items.map((item, i) =>
-        <Accordion key={i} expanded={expanded === item.title} onChange={handleChange(item.title)}>
+    <AccordionWrapper>
+      <div className="inline-flex items-center mb-4">
+        <IconAvatar>
+          <div className="w-4 h-4 flex justify-center">
+            <CardIcon />
+          </div>
+        </IconAvatar>
+        <Typography variant="subtitle1" className="pl-2">
+          {group.title}
+        </Typography>
+      </div>
+      {group.items.map((item, i) => (
+        <Accordion
+          className="pp"
+          key={i}
+          expanded={expanded === item.title}
+          onChange={handleChange(item.title)}
+        >
           <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
             <Typography>{item.title}</Typography>
           </AccordionSummary>
-          <AccordionDetails>
-            {item.content}
-          </AccordionDetails>
+          <AccordionDetails>{item.content}</AccordionDetails>
         </Accordion>
-      )}
-    </div>
+      ))}
+    </AccordionWrapper>
   );
-}
+};
--- client/did-web-service/src/app/(pages)/support/faq/page.tsx
+++ client/did-web-service/src/app/(pages)/support/faq/page.tsx
@@ -1,27 +1,29 @@
 "use client";
-import { Typography } from "@mui/material";
 import { FC } from "react";
+import { Stack, Typography } from "@mui/material";
 import { FAQ } from "./FAQ";
 import { FAQDataDID } from "./faq-data-did";
 import { FAQDataMydentity } from "./faq-data-mydentity";
 import { FAQGroup } from "./faq-types";
+import Headline from "@components/layout/Headline";
 
-const faqEntries: FAQGroup[] = [
-  FAQDataDID,
-  FAQDataMydentity
-]
+const faqEntries: FAQGroup[] = [FAQDataDID, FAQDataMydentity];
 
 const FAQPage: FC = () => {
-  return (<div className="col-span-full">
-    <Typography variant="h3">
-      F.A.Q.
-    </Typography>
-    <Typography>
-      You have many questions about these new "identities"? We might have answers.
-    </Typography>
-
-    {faqEntries.map((group, i) => <FAQ className="mt-6" key={i} group={group} />)}
-  </div>)
-}
+  return (
+    <div>
+      <Headline
+        title="Frequently Asked Questions"
+        description="Do you have questions about these new 'identities'? We are here with answers to address any queries you may have."
+        showBg={true}
+      />
+      <Stack spacing={2}>
+        {faqEntries.map((group, i) => (
+          <FAQ key={i} group={group} />
+        ))}
+      </Stack>
+    </div>
+  );
+};
 
 export default FAQPage;
